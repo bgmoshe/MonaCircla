8eb592053695c39299ba23523e52b5ce1dc927a3
bgmoshe <barakgross5@gmail.com>
2019-06-22 12:57:03 +0300

    Updated circle class to not save contained points by default and made the calculation of checking contained points to use prexistenig function

--- circle.py
+++ circle.py
@@ -5,6 +5,7 @@ Created on Sat Jun  8 15:02:00 2019
 @author: barak
 """
 
+from random import uniform
 
 class Circle:
     
@@ -14,7 +15,7 @@ class Circle:
         self.y_center = y_center
         self.points = list()
 
-    def getAllContainedIntegerPoints(self, save_points):
+    def getAllContainedIntegerPoints(self, save_points=False):
         points = list()
 
         #To reduce number of calculations
@@ -22,14 +23,25 @@ class Circle:
             return self.points
 
         for offset in range(-1*self.radius, self.radius+1):
-            squared_y_delta = self.radius**2 - (self.x_center + offset)**2
             current_x = self.x_center + offset
             new_points = [(current_x, y)
-                          for y in range(self.y_center-self.radius, self.y_center+self.radius)
-                          if (y-self.y_center)**2 <= squared_y_delta]
+                          for y in range(self.y_center-self.radius, self.y_center+self.radius+1)
+                          if self.check_if_contains_point(current_x, y)]
             points = points + new_points
 
         if save_points:
             self.points = points
 
         return points
+
+    def check_if_contains_point(self, x, y):
+        distance = (self.x_center-x) ** 2 + (self.y_center-y) ** 2
+        return distance <= self.radius ** 2
+
+
+def generate_random_circle(width, height, max_radius):
+    x = uniform(0, width - 0.1)
+    y = uniform(0, height - 0.1)
+    r = uniform(1, max_radius+1)
+    circle = Circle(x_center=int(x), y_center=int(y), radius=int(r))
+    return circle
